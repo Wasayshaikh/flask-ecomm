bce2ef4372939481a4d0d875173d7449f0155b5b
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-01-31 18:47:11 +0500

    set up SQLALCHEMY

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from routes.web import web
+from application import models
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/blog_post'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+models.db.init_app(app)
 
 app.register_blueprint(web, url_prefix='/')
 
@@ -9,6 +14,7 @@ def page_not_found(e):
     # note that we set the 404 status explicitly
     return '404.html'
 
-
+with app.app_context():
+    models.db.create_all()
 if __name__ == '__main__':
     app.run(debug=True)
