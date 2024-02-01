547b107f88298aab27641eb0aa71d264f98311dc
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-01 15:59:52 +0500

    listing all the files

--- application/controllers/__init__.py
+++ application/controllers/__init__.py
@@ -1,3 +1,5 @@
 from application.controllers.HomeController import HomeController 
-
+from application.controllers.AuthController import AuthController 
+from application.controllers.Controller import Controller
+auth = AuthController()
 home = HomeController()
