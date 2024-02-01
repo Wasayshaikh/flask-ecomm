8310d62c01a7903063fbacced7a05eecdae88bfe
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-01 16:04:41 +0500

    add auth routes

--- routes/web.py
+++ routes/web.py
@@ -1,5 +1,9 @@
 from flask import Blueprint
-from application.controllers import home
+from application.controllers import home, auth
 web = Blueprint('web',__name__)
 
 web.add_url_rule('/',view_func=home.index, methods=['GET'])
+
+#auth Routes
+web.add_url_rule('/login',view_func=auth.login, methods=['GET',"POST"])
+web.add_url_rule('/register',view_func=auth.register, methods=['GET',"POST"])
