b61ac15332a99b326888eee1d9826313546045db
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-01 16:00:14 +0500

    auth controller add data in database

--- application/controllers/AuthController.py
+++ application/controllers/AuthController.py
@@ -1,3 +1,29 @@
-class AuthController:
-    def login_get(self):
-        pass
+from .Controller import Controller
+from application.models import ModelClasses
+from application.models import session
+
+
+class AuthController(Controller):
+    def login(self):
+        request_type=super().request().method
+        if request_type == "GET":
+            data = {"title": "Login"}
+            return super().render("auth/login", data)
+    def register(self):
+        request_type = super().request().method
+        if request_type == "GET":
+            data = {"title": "Register"}
+            return super().render("auth/register", data)
+        else:
+            user_data = {
+                "username": super().request().form['username'],
+                "email": super().request().form['email'],
+                "password": super().request().form['password'],
+                "first_name": super().request().form['first_name'],
+                "last_name": super().request().form['last_name']
+            }
+            new_user = ModelClasses.Users(**user_data)
+            session.add(new_user)
+            session.commit()
+            session.close()
+            return super().redirect("web.login")
