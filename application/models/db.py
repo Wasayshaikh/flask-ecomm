b53f554dc9c5b74280250efa67c01adf070f7898
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-01 16:01:16 +0500

    db file with session created

--- application/models/db.py
+++ application/models/db.py
@@ -1,3 +1,17 @@
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+#db = SQLAlchemy()
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base  = declarative_base()
+
+def create_session(db_uri):
+    engine = create_engine(db_uri)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    return Session()
+
+database_uri = 'postgresql://postgres:1234@localhost/blog_post'
+session = create_session(database_uri)
