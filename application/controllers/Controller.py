4740a02348e3c9e76fa01cb9ae8a29b75456e605
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-01 16:00:24 +0500

    add redirect function

--- application/controllers/Controller.py
+++ application/controllers/Controller.py
@@ -1,8 +1,13 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 class Controller:
     def __init__(self) -> None:
        pass 
-    def render(self,template, data):
-        return render_template(template,**data)
+    def render(self,template, data=None):
+        if data is None:
+            return render_template("pages/"+template + ".html")
+        else:
+            return render_template("pages/"+template + ".html",**data)
     def request(self):
         return request
+    def redirect(self, blue_print):
+        return redirect(url_for(blue_print))
