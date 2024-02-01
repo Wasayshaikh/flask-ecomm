54f5c2579238ee6c8a28c05f4e6b418b37506bb8
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-01 15:59:37 +0500

    using simple sqlAchemy for database connection

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from routes.web import web
-from application import models
+from application.models import create_session,database_uri
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/blog_post'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-models.db.init_app(app)
+
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# models.db.init_app(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost/blog_post'
+
+session = create_session(database_uri)
+# Create a session to interact with the database
+
 
 app.register_blueprint(web, url_prefix='/')
 
@@ -14,7 +19,8 @@ def page_not_found(e):
     # note that we set the 404 status explicitly
     return '404.html'
 
-with app.app_context():
-    models.db.create_all()
+#with app.app_context():
+    #models.db.create_all()
 if __name__ == '__main__':
+    
     app.run(debug=True)
