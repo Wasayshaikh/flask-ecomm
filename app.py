a0d8c16e8d86b3fb7dce33813d9a81eebc0ff15c
abdul wasay <abdulwasayshaikh1999@gmail.com>
2024-02-05 20:47:04 +0500

    add csrf validation

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from routes.web import web
 from application.models import create_session,database_uri
+from flask_wtf.csrf import CSRFProtect
 app = Flask(__name__)
 
 
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # models.db.init_app(app)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost/blog_post'
-
-session = create_session(database_uri)
+app.secret_key = "123456789"
+app.config['SECRET_KEY'] = 'your_secret_key'
+databaseSession = create_session(database_uri)
 # Create a session to interact with the database
 
-
+csrf = CSRFProtect(app)
 app.register_blueprint(web, url_prefix='/')
 
 @app.errorhandler(404)
