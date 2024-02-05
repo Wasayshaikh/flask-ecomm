3c380ca399232e79104b35d17a96f50bc00a70c5
abdul wasay <abdulwasayshaikh1999@gmail.com>
2024-02-05 20:47:55 +0500

    add database to mysql connector

--- routes/web.py
+++ routes/web.py
@@ -7,3 +7,4 @@ web.add_url_rule('/',view_func=home.index, methods=['GET'])
 #auth Routes
 web.add_url_rule('/login',view_func=auth.login, methods=['GET',"POST"])
 web.add_url_rule('/register',view_func=auth.register, methods=['GET',"POST"])
+web.add_url_rule('/logout',view_func=auth.logout,methods=["GET","POST"])
