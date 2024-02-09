f0e85a57c0f54883d1f22b4a4b513ea6c0d53bd3
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-09 21:03:33 +0500

    add wrong credentials check

--- application/controllers/AuthController.py
+++ application/controllers/AuthController.py
@@ -1,54 +1,65 @@
 from .Controller import Controller
-from application.models import ModelClasses
+from application.models import Users
 from application.models import databaseSession
-from flask import session, jsonify
+from flask import session
+from sqlalchemy.exc import IntegrityError
 
 
 class AuthController(Controller):
     def login(self):
-        request_type=super().request().method
+        request_type=self.request().method
         if request_type == "GET":
             if 'auth_user' in session:
-                return super().redirect("web.index")
+                return self.redirect("web.index")
             else:
                 data = {"title": "Login"}
-                return super().render("auth/login", data)
+                return self.render("auth/login", data)
         else:
-            username = super().request().form['username']
-            password = super().request().form['password']
-            dataUser = databaseSession.query(ModelClasses.Users).where(ModelClasses.Users.username == username).where(ModelClasses.Users.password == password).first()
-            session['auth_user'] = {"username": dataUser.username, "id":dataUser.id, "email":dataUser.email}
-            print(dataUser)
-            return super().redirect("web.index")
+            username = self.request().form['username']
+            password = self.request().form['password']
+            #dataUser = databaseSession.query(ModelClasses.Users).where(ModelClasses.Users.username == username).where(ModelClasses.Users.password == password)
+            dataUser = databaseSession.query(Users).filter(Users.username == username,Users.password == password).first()
+            # session['auth_user'] = {"username": dataUser.username, "id":dataUser.id, "email":dataUser.email}
+            if dataUser:
+                return self.redirect("web.index")
+            else:
+                self.flash_error("Invalid credentials")
+                return self.redirect("web.login")
 
     def register(self):
-        request_type = super().request().method
+        request_type = self.request().method
         if request_type == "GET":
             if 'auth_user' in session:
-                return super().redirect("web.index")
+                return self.redirect("web.index")
             else:
                 data = {"title": "Register"}
-                return super().render("auth/register", data)
+                return self.render("auth/register", data)
         else:
-            user_data = {
-                "username": super().request().form['username'],
-                "email": super().request().form['email'],
-                "password": super().request().form['password'],
-                "first_name": super().request().form['first_name'],
-                "last_name": super().request().form['last_name']
-            }
-            new_user = ModelClasses.Users(**user_data)
-            databaseSession.add(new_user)
-            databaseSession.commit()
-            databaseSession.close()
-            return super().redirect("web.login")
+            try:
+                user_data = {
+                    "username": self.request().form['username'],
+                    "email": self.request().form['email'],
+                    "password": self.request().form['password'],
+                    "first_name": self.request().form['first_name'],
+                    "last_name": self.request().form['last_name']
+                }
+                new_user = Users(**user_data)
+                databaseSession.add(new_user)
+                databaseSession.commit()
+                databaseSession.close()
+                return self.redirect("web.login")
+            except IntegrityError as e:
+                databaseSession.rollback()
+                print("Error: Data could not be stored in the database due to integrity constraints.")
+                self.flash_error("username or email already exist")
+                return self.redirect('web.register')
     def logout(self):
-        request_type=super().request().method
+        request_type=self.request().method
         if request_type == "GET":
             if 'auth_user' in session:
                 session.pop("auth_user",None)
-                return super().redirect("web.login")
+                return self.redirect("web.login")
             else:
-                return super().redirect('web.index')
+                return self.redirect('web.index')
         else:
-            return super().redirect('web.index')
+            return self.redirect('web.index')
