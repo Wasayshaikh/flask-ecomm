a4b7ee3686943d05cc5ea4819950d1cb931adb6f
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-12 15:51:08 +0500

    set the import correctly

--- application/controllers/__init__.py
+++ application/controllers/__init__.py
@@ -1,5 +1,4 @@
 from application.controllers.HomeController import HomeController 
 from application.controllers.AuthController import AuthController 
-from application.controllers.Controller import Controller
 auth = AuthController()
 home = HomeController()
--- application/models/__init__.py
+++ application/models/__init__.py
@@ -1,2 +1,2 @@
 from application.models.db import Base, databaseSession,create_session,database_uri
-from application.models.ModelClasses import Users
+from application.models.User import Users
