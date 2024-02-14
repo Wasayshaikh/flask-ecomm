1c961a11c164835e0355b8cd9d8f4a2a9eed0d9d
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-14 14:26:52 +0500

    product and product_meta model

--- application/models/Product.py
+++ application/models/Product.py
@@ -1,8 +1,15 @@
-from models import Base
+from .db import Base
 from sqlalchemy import Column, BigInteger, Integer, String,Text
+from sqlalchemy.orm import relationship
 class Products(Base):
-    id= Column(Integer(30),primary_key=True, autoincrement=True )
-    title=Column(String(100), nullable=False)
-    slug= Column(String(100), nullable=False)
+    __tablename__ = "products"
+    id= Column(Integer,primary_key=True, autoincrement=True )
+    product_code = Column(String(100), unique=True)
+    slug= Column(String(100), nullable=False, unique=True)
     user_id= Column(BigInteger, nullable=True)
+    is_active= Column(BigInteger, default=0)
+    stocks= Column(BigInteger, nullable=True)
+    out_of_stock = Column(BigInteger, default=0)
+    product_metadata = relationship("ProductMetaData", uselist=False, back_populates="product", cascade="all, delete-orphan")
+
     
---
+++ application/models/ProductMetaData.py
@@ -0,0 +1,11 @@
+from .db import Base
+from sqlalchemy import Column, BigInteger, Integer, String,Text, ForeignKey
+from sqlalchemy.orm import relationship
+class ProductMetaData(Base):
+    __tablename__ = "product_metadata"
+    id= Column(Integer,primary_key=True, autoincrement=True )
+    product_id = Column(Integer, ForeignKey("products.id"), unique=True)
+    title=Column(String(100), nullable=False)
+    thumbnail = Column(Text, nullable=False)
+    images = Column(Text, nullable=True)
+    product = relationship("Products", back_populates="product_metadata")
