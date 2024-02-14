643d4f60b28f2326b15731a9e47e40f6821ab2db
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-14 19:15:14 +0500

    add dashboard controller

--- application/controllers/__init__.py
+++ application/controllers/__init__.py
@@ -1,6 +1,8 @@
 from application.controllers.HomeController import HomeController 
 from application.controllers.AuthController import AuthController 
 from application.controllers.ProductController import ProductController
+from application.controllers.DashboardController import DashboardController
 auth = AuthController()
 home = HomeController()
 product = ProductController()
+dashboard = DashboardController()
