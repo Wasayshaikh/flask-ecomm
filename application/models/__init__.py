67ea3aa2d18ad8043aa6f4633a5b4930e8737b7c
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-14 14:26:27 +0500

    add product and product_meta model

--- application/models/__init__.py
+++ application/models/__init__.py
@@ -1,2 +1,4 @@
 from application.models.db import Base, databaseSession,create_session,database_uri
 from application.models.User import Users
+from application.models.Product import Products
+from application.models.ProductMetaData import ProductMetaData
