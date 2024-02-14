be0df9adbd760955ea352b0208c0eacc506007b2
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-14 14:26:06 +0500

    add productController

--- application/controllers/__init__.py
+++ application/controllers/__init__.py
@@ -1,4 +1,6 @@
 from application.controllers.HomeController import HomeController 
 from application.controllers.AuthController import AuthController 
+from application.controllers.ProductController import ProductController
 auth = AuthController()
 home = HomeController()
+product = ProductController()
