44ccaa944aae3e5a2ea9a1445247aa741a1e549e
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-14 14:26:14 +0500

    productController

---
+++ application/controllers/ProductController.py
@@ -0,0 +1,12 @@
+from application.controllers.Controller import Controller
+class ProductController(Controller):
+    def __init__(self):
+        pass
+    def get_product(self):
+        pass
+    def add_product(self):
+        pass
+    def edit_product(self):
+        pass
+    def delete_product(self):
+        pass
