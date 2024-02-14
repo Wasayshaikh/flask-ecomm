5480451af46481e631e1eb44faa749a89778bd67
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-14 19:16:00 +0500

    add dashboard routes

--- routes/web.py
+++ routes/web.py
@@ -1,10 +1,13 @@
 from flask import Blueprint
-from application.controllers import home, auth
+from application.controllers import home, auth,dashboard,product
 web = Blueprint('web',__name__)
 
-web.add_url_rule('/',view_func=home.index, methods=['GET'])
+web.add_url_rule('/',view_func=home.index, methods=['GET',"POST"])
 
 #auth Routes
 web.add_url_rule('/login',view_func=auth.login, methods=['GET',"POST"])
 web.add_url_rule('/register',view_func=auth.register, methods=['GET',"POST"])
 web.add_url_rule('/logout',view_func=auth.logout,methods=["GET","POST"])
+
+#dashboard Routes
+web.add_url_rule('/dashboard',view_func=dashboard.dashboard, methods=['GET',"POST"])
