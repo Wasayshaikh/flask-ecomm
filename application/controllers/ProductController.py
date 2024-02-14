86e837d15e022162f33e5a4192488f307f17eebc
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-14 20:47:04 +0500

    all product methods

--- application/controllers/ProductController.py
+++ application/controllers/ProductController.py
@@ -1,9 +1,12 @@
 from application.controllers.Controller import Controller
+from application.models import databaseSession, Products, ProductMetaData
 class ProductController(Controller):
     def __init__(self):
         pass
-    def get_product(self):
-        pass
+    def products(self):
+        products = databaseSession.query(Products).all()
+        data = {"title":"Products", "products":products}
+        return self.render("dashboard/product/product", data)
     def add_product(self):
         pass
     def edit_product(self):
