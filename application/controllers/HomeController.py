1e6f70a52ee5237ab834f13b734092aeefcea6c6
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-16 16:04:31 +0500

    get products

--- application/controllers/HomeController.py
+++ application/controllers/HomeController.py
@@ -1,9 +1,11 @@
 from .Controller import Controller
+from application.models import Products, databaseSession
 class HomeController(Controller): 
     def __init__(self) -> None:
         self.name = "this name"
         pass
     
     def index(self):
-        data = {'title': "homes"}
+        products = databaseSession.query(Products).all()
+        data = {'title': "homes", "products":products}
         return super().render('index', data)
