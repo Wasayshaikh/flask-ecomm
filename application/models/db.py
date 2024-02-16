84683dcb4adbfa7d0a0a3a4d9a10ed525a49bd9c
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-02-16 16:04:53 +0500

    use env variables

--- application/models/db.py
+++ application/models/db.py
@@ -4,10 +4,33 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from application.helper import env
+# Access the environment variables
+database_uri = ''
+database_port = ""
+database_password=""
+DATABASE_ENG = env("DATABASE_ENG")
+database_url = env("DATABASE_URL")
+database_user = env("DATABASE_USER")
+password = env("DATABASE_PASSWORD")
+database = env("DATABASE")
+port =  env("DATABASE_PORT")
+
+if port != "":
+    database_port =  ":"+port
+if password != "":
+    database_password = ":"+password
+
+
+
+if DATABASE_ENG == "postgre":
+    database_uri = f"postgresql://{database_user}{database_password}@{database_url}{database_port}/{database}"
+elif DATABASE_ENG == "mysql":
+    database_uri = f"mysql+mysqlconnector://{database_user}{database_password}@{database_url}{database_port}/{database}"
+elif DATABASE_ENG == "sqlite":
+    database_uri = f"sqlite:///{database}"
 
 Base  = declarative_base()
-database_uri = 'postgresql://postgres:1234@localhost/blog_post'
-# database_uri = 'mysql+mysqlconnector://root:1234@localhost/blog_post'
 engine = create_engine(database_uri)
 def create_tables():
     Base.metadata.create_all(engine)
